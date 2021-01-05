7500143404f081a974afa261bdcc2e1bc1711a31
cory <cory1993johnson@gmail.com>
2021-01-04 18:33:15 -0800

    adding apihitter for open weather

---
+++ src/apihitterr/apihitter.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def open_weather_hitter(lat, lon):
+    url_base    = 'http://api.openweathermap.org/data/2.5/'
+    token       = '1a4d180ffc35d8e5f8cd405d6caf99f1'
+
+    header = {}
+    payload = {'appid': token, 'lat': lat, 'lon': lon}   
+    result = requests.get(url_base + 'weather', params=payload, headers=header)
+    
+    return result.json()
+    pass
+
+if __name__ == "__main__":
+    x = open_weather_hitter(45.330558, -121.711615)
+    print(x)
+
+    pass
