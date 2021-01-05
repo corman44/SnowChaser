11103009a23a6f49e43923d63742c1bcd022929b
Tyler Miller <tylermillerhome@gmail.com>
2021-01-04 18:17:36 -0800

    renamed index route to conditions

--- PowProducer/PowProducer/urls.py
+++ PowProducer/PowProducer/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snowview/', include('snowview.urls')),    
+    path('conditions', include('conditions.urls')),
 ]
