0f331ddcab6e1d20e48c36e9c382f969e3228f70
Tyler Miller <tylermillerhome@gmail.com>
2021-01-04 17:59:44 -0800

    Create Django web server, commence pow production

--- PowProducer/PowProducer/asgi.py
+++ PowProducer/PowProducer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PowFinder project.
+ASGI config for PowProducer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PowFinder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PowProducer.settings')
 
 application = get_asgi_application()
