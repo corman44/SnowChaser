d6c3cd67f1b1b81cc708f4fcc49ba6bd5f14728a
cory <cory1993johnson@gmail.com>
2021-01-04 18:39:33 -0800

    include token for NOAA

--- src/apihitterr/apihitter.py
+++ src/apihitterr/apihitter.py
@@ -1,6 +1,9 @@
 import requests
 
 def open_weather_hitter(lat, lon):
+    """
+    Get weather data from OpenWeather API
+    """
     url_base    = 'http://api.openweathermap.org/data/2.5/'
     token       = '1a4d180ffc35d8e5f8cd405d6caf99f1'
 
@@ -12,8 +15,12 @@ def open_weather_hitter(lat, lon):
 
 def noaa_hitter(lat, lon):
     """
-    to access NOAA API Weather data
+    Get weather data from NOAA API (not defined)
     """
+    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/'
+    token = 'LfxZeKJJIQXTfiomXFDkBLrFbiXuARcL'
+    dataset_endpoint = '/datasets'
+    
     pass
 
 if __name__ == "__main__":
